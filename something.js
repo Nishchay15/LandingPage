29cf17971495eba221c99cee5d118a5f3fa70c8c
Nishchay15 <146799557+Nishchay15@users.noreply.github.com>
2023-12-01 12:41:34 +0530

    Update something.js

--- something.js
+++ something.js
@@ -1,55 +1,41 @@
-function submitForm() {
-    var name = document.getElementById('name').value;
-   alert('Your Name is: ' + name);
- }
 
-//check phone number valid/invalid
- function checkphoneNumber(phoneNumber) {
-    if (phoneNumber.match(/\d/g).length !== 10) {
-        alert("your number is invalid");
-    } else {
-        alert("your number is valid");
-    }
-}
- 
-//validating email id
-function validateEmail() {
-    var email = document.getElementById("email").value;
-    var invalidDomains = ["gmail.com", "hotmail.com", "yahoo.com"];
-    var isInvalid = false;
-
-    for (var i = 0; i < invalidDomains.length; i++) {
-        if (email.includes(invalidDomains[i])) {
-            isInvalid = true;
-            break;
+        function submitForm() {
             
+            var name = document.getElementById("name").value;
+            var email = document.getElementById("email").value;
+            var phone = document.getElementById("phone").value;
+
+            
+            alert("Your name is " + name);
+
+            
+            if (phone.length !== 10) {
+                alert("Your number is invalid");
+                return;
+            }
+
+            
+            var validDomains = ["gmail.com", "hotmail.com", "yahoo.com"];
+            var isValidDomain = false;
+
+            for (var i = 0; i < validDomains.length; i++) {
+                if (email.includes(validDomains[i])) {
+                    isValidDomain = true;
+                    break;
+                }
+            }
+
+            if (isValidDomain) {
+                alert("Please enter your official mail id");
+                return;
+            }
+
+            
+            var bannerText = "Thank you " + name + " for sharing your details. We will reach out to you on your mail id: " + email;
+            document.getElementById("banner").innerText = bannerText;
         }
-    }   
 
-    if (isInvalid) {
-        alert("Please enter your official mail id");
-    }
-}
-
-function showThankYou(name, email) {
-    var banner = document.getElementsByClassName('banner');
-    if (banner) {
-       banner.textContent = 'Thank you ' + nameValue + ' for sharing your details. We will reach out to you on your mail id: ' + emailValue;
-     
-    }
-   showThankYou(name,email)
-}
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
+        function submitContactForm() {
+           
+            alert("Contact form submitted!");
+        }
